7e13a1636ea0966f9f945c417376e6b6f2d0003d
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2024-07-17 22:49:10 +0900

    fix: 関数化

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,21 @@ use std::{collections::BTreeMap, env, error::Error, fs::File, io::{BufRead, BufR
 use dotenv::dotenv;
 fn main() -> Result<(),Box<dyn Error>> {
     dotenv().ok();
-    let mut sysctl_map:BTreeMap<usize,(bool,String)> = BTreeMap::new();
     let sysctl_file_path = env::var("SYSCTL_FILE_PATH").expect("SYSCTL_FILE_PATH must be set");
     let sysctl_file_content = read_file(&sysctl_file_path)?;
+    let sysctl_map = parse_sysctl_content(sysctl_file_content)?;
+    println!("{:?}",sysctl_map);
+    Ok(())
+}
+
+fn read_file(path: &str) -> Result<BufReader<File>,Box<dyn Error>>{
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    Ok(reader)
+}
+
+fn parse_sysctl_content(sysctl_file_content: BufReader<File>) -> Result<BTreeMap<usize,(bool,String)>,Box<dyn Error>>{
+    let mut sysctl_map:BTreeMap<usize,(bool,String)> = BTreeMap::new();
     for (index,line) in sysctl_file_content.lines().enumerate(){
         let line = line?;
         if line.trim().is_empty(){
@@ -17,14 +29,7 @@ fn main() -> Result<(),Box<dyn Error>> {
             Some('#') => true,
             _ => false,
         };
-        sysctl_map.insert(index, (is_comment,line));
+        sysctl_map.insert(index + 1, (is_comment,line));
     }
-    println!("{:?}",sysctl_map);
-    Ok(())
-}
-
-fn read_file(path: &str) -> Result<BufReader<File>,Box<dyn Error>>{
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-    Ok(reader)
+    Ok(sysctl_map)
 }
